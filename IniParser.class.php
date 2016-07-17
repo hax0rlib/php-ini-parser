0c93d2733ea0709ac0b22f48a36a5270b49cc04c
André Rodriguês <andretutors0@gmail.com>
2016-07-17 18:42:16 -0300

    New documentation

--- IniParser.class.php
+++ IniParser.class.php
@@ -1,86 +1,77 @@
 <?php
 
 /**
- * Name: IniParser 1.0
- * Author: Eduardo Arruda
- * Website: http://www.webgit.com.br/
- * Creation date: 11/06/2016
+ * @package IniParser 1.0
+ * @author Eduardo Arruda <http://www.webgit.com.br/>
+ * @since 11/06/2016
+ * @version 1.0
  *
- * IMPORTANT: 
+ * IMPORTANT:
  * - Remember to change the default dir on the constructor.
  */
-
-class IniParser {
+class IniParser
+{
 
     private $dir;
     private $method;
     private $path;
 
     /**
-     * <b>Class Constructor</b>
-     *
-     * @var BOOLEAN $return_object (default: false) - Return method (false = array, true = object)
-     * @return n/a
+     * IniParser constructor.
+     * @param bool $return_object
      */
-    
     public function __construct($return_object = false)
-    {   
+    {
         $this->dir = "./accounts/";
         $this->method = $return_object;
     }
 
-    /**
-     * <b>File Finder</b>
-     * 
-     * Check if the file exists
-     *
-     * @var STRING $file_name - The name of the .ini file (ex. PlayerName)
-     * @return boolean
-     */
 
-    public function FileExists($file_name)
+    /**
+     * function for file validation.
+     *
+     * @param $file_name
+     * @return bool
+     */
+    public function fileExists($file_name)
     {
         $this->path = "{$this->dir}{$file_name}.ini";
         if (file_exists($this->path) && !is_dir($this->path))
-       	{
+        {
             return true;
         }
     }
 
-    /**
-     * <b>File Getter</b>
-     * 
-     * Return an array or object with all the data in the .ini file.
-     *
-     * @var STRING $file_name - The name of the .ini file (ex. PlayerName)
-     * @return array or object
-     */
 
-    public function GetFileContent($file_name)
+    /**
+     * function to generate file.
+     *
+     * @param $file_name
+     * @return array|object
+     */
+    public function getFileContent($file_name)
     {
         $this->path = "{$this->dir}{$file_name}.ini";
-        if ($this->FileExists($file_name))
+        if ($this->fileExists($file_name))
         {
-        	$res = array();
+            $res = array();
             $parse_arr = parse_ini_file($this->path, false);
             foreach($parse_arr as $key => $val)
             {
-            	$res[$key] = is_numeric($val) ? intval($val) : $val;
+                $res[$key] = is_numeric($val) ? intval($val) : $val;
             }
             return ($this->method) ? (object) $res : $res;
         }
     }
 
-    /**
-     * <b>File Delete</b>
-     * 
-     * Delete the specified file.
-     *
-     * @var STRING $file_name - The name of the .ini file (ex. PlayerName)
-     * @return boolean
-     */
 
-    public function DeleteFile($file_name)
+    /**
+     * function to delete file.
+     *
+     * @param $file_name
+     * @return bool
+     */
+    public function deleteFile($file_name)
     {
         $this->path = "{$this->dir}{$file_name}.ini";
         if ($this->FileExists($file_name))
@@ -92,72 +83,63 @@ class IniParser {
         }
     }
 
+
     /**
-     * <b>File Update</b>
-     * 
-     * Update the specified .ini file with parameters in the array.
+     * function to update file.
      *
-     * @var ARRAY $array - Array with the data to update (ex. array('money' => 300))
-     * @var STRING $file_name - The name of the .ini file (ex. PlayerName)
-     * @return n/a
+     * @param $array
+     * @param $file_name
      */
-
-    public function UpdateFile($array, $file_name)
+    public function updateFile($array, $file_name)
     {
-    	$this_ac = $this->GetFileContent($file_name, true);
-    	if($this_ac)
-    	{
-    		$res = array();
-	        $keys = array();
+        $this_ac = $this->getFileContent($file_name, true);
+        if($this_ac)
+        {
+            $res = array();
+            $keys = array();
 
-	        foreach($array as $key => $val)
-	        {
-				$res[] = "$key = " . (is_numeric($val) ? $val : '"' . $val . '"');
-				$keys[] = $key;
-	        }
-	        $keys_string  = implode($keys, '/');
-			foreach($this_ac as $a_key => $a_val)
-	    	{
-	    		if(strpos($keys_string, $a_key) === false)
-	    		{
-	    			$res[] = "$a_key = " . (is_numeric($a_val) ? $a_val : '"' . $a_val . '"');
-	    		}
-	    	}
-	        $this->SaveFile($file_name, implode("\r\n", $res));
-    	}
+            foreach($array as $key => $val)
+            {
+                $res[] = "$key = " . (is_numeric($val) ? $val : '"' . $val . '"');
+                $keys[] = $key;
+            }
+            $keys_string  = implode($keys, '/');
+            foreach($this_ac as $a_key => $a_val)
+            {
+                if(strpos($keys_string, $a_key) === false)
+                {
+                    $res[] = "$a_key = " . (is_numeric($a_val) ? $a_val : '"' . $a_val . '"');
+                }
+            }
+            $this->saveFile($file_name, implode("\r\n", $res));
+        }
     }
 
-    /**
-     * <b>File Create</b>
-     * 
-     * Create an .ini file in the specified path with array data.
-     *
-     * @var ARRAY $array - Array with the data to insert (ex. array('money' => 300))
-     * @var STRING $file_name - The name of the .ini file (ex. PlayerName)
-     * @return n/a
-     */
 
-    public function CreateFile($array, $file_name)
+    /**
+     * function to create file.
+     *
+     * @param $array
+     * @param $file_name
+     */
+    public function createFile($array, $file_name)
     {
         $res = array();
         foreach($array as $key => $val)
         {
             $res[] = "$key = " . (is_numeric($val) ? $val : '"' . $val . '"');
         }
-        $this->SaveFile($file_name, implode("\r\n", $res));
+        $this->saveFile($file_name, implode("\r\n", $res));
     }
 
-    /**
-     * <b>File Saver</b>
-     * 
-     * Private function who save changes in the .ini file.
-     *
-     * @var STRING $file_name - The name of the .ini file (ex. PlayerName)
-     * @var STRING $dataToSave - String of the implode of the arrayData.
-     * @return n/a
-     */
 
-    private function SaveFile($file_name, $dataToSave)
+    /**
+     * function to save file.
+     * 
+     * @param $file_name
+     * @param $dataToSave
+     */
+    private function saveFile($file_name, $dataToSave)
     {
         $this->path = "{$this->dir}{$file_name}.ini";
         if ($fp = fopen($this->path, 'w'))
